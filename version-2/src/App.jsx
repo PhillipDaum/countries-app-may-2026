0e249d8aa594446c0be6a19cc2ad1d309ea51b24
PhillipDaum <philgdaum@gmail.com>
2026-07-06 13:11:15 -0600

    switch from REST Countries API to countries.dev

--- version-2/src/App.jsx
+++ version-2/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   const getAllCountriesFromApi = async () => {
     try {
       const response = await fetch(
-        "https://restcountries.com/v3.1/all?fields=name,capital,population,region,currencies,borders,flags,cca3",
+        "https://countries.dev/countries?fields=name,capital,population,region,currencies,borders,flags,cca3",
       );
       const data = await response.json();
       setAllCountries(
---
+++ version-2/src/pages/SavedCountries.jsx
@@ -0,0 +1,59 @@
+export default function SavedCountries({ allCountries }) {
+  
+  console.log("from Saved Countries", allCountries)
+
+
+  return (
+    <>
+      <h2>My Saved Countries</h2>
+      <form>
+        <fieldset>
+          <legend>My Profile</legend>
+
+          <label htmlFor="fullname" className="visually-hidden">
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="fullname"
+            name="fullname"
+            placeholder="Full Name"
+            required
+          />
+
+          <label htmlFor="email" className="visually-hidden">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+
+          <label htmlFor="country" className="visually-hidden">
+            Country
+          </label>
+          <input
+            type="text"
+            id="country"
+            name="country"
+            placeholder="Country"
+          />
+
+          <label htmlFor="bio" className="visually-hidden">
+            Bio
+          </label>
+          <textarea
+            name="bio"
+            id="bio"
+            rows="10"
+            placeholder="Bio"
+          ></textarea>
+        </fieldset>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
